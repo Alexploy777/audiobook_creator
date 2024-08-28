a8dfe844e806e4d0a78c8ac60e7c039eaf6807b6
AlexItalbazar <a-egorov@bk.ru>
2024-08-28 15:31:51 +0300

    Главный модуль
    создан главный модуль с примерной структурой методов

--- main.py
+++ main.py
@@ -10,7 +10,32 @@ class AudiobookCreator(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.setWindowTitle('Audiobook Creator')
 
+        # Подключаем сигналы к слотам
+        self.pushButton.clicked.connect(self.add_files)
+        self.pushButton_2.clicked.connect(self.remove_selected_files)
+        self.pushButton_upload_cover.clicked.connect(self.upload_cover)
+        self.pushButton_convert.clicked.connect(self.convert_files)
+        self.pushButton_stop_and_clean.clicked.connect(self.stop_and_clean)
 
+    def add_files(self):
+        # Реализация добавления файлов в listWidget
+        pass
+
+    def remove_selected_files(self):
+        # Реализация удаления выбранных файлов из listWidget
+        pass
+
+    def upload_cover(self):
+        # Реализация загрузки обложки
+        pass
+
+    def convert_files(self):
+        # Реализация конвертации файлов
+        pass
+
+    def stop_and_clean(self):
+        # Реализация остановки процесса и очистки данных
+        pass
 
 
 if __name__ == '__main__':
