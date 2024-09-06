48ce42a6decdbe4fab7005632460adcdc23aa14c
AlexItalbazar <a-egorov@bk.ru>
2024-09-06 16:07:18 +0300

    модуль audio_processing.py
    на данный момент происходит корректная конвертация с помещением метаданных и обложки

--- core/audio_processing.py
+++ core/audio_processing.py
@@ -7,60 +7,97 @@ class AudioProcessor:
 
     def convert_and_combine(self, file_paths, output_path, bitrate, metadata, cover_image, progress_callback):
         global cover_image_path
+        # try:
+        # Создаем временный список для всех аудиофайлов
+        temp_list = "file_list.txt"
+        with open(temp_list, 'w', encoding='utf-8') as f:
+            for file_path in file_paths:
+                f.write(f"file '{file_path}'\n")
+
+        # Команда ffmpeg для объединения аудио файлов
+        intermediate_audio = 'intermediate.mp3'
+        command = [
+            self.ffmpeg_path, '-y', '-f', 'concat', '-safe', '0', '-i', temp_list,
+            '-c', 'copy'
+        ]
+
+        # Добавляем метаданные к команде (они должны быть ДО вывода файла)
+        if metadata["title"]:
+            command.extend(['-metadata', f"title={metadata['title']}"])
+        if metadata["artist"]:
+            command.extend(['-metadata', f"artist={metadata['artist']}"])
+        if metadata["album"]:
+            command.extend(['-metadata', f"album={metadata['album']}"])
+        if metadata["year"]:
+            command.extend(['-metadata', f"year={metadata['year']}"])
+        if metadata["genre"]:
+            command.extend(['-metadata', f"genre={metadata['genre']}"])
+
+        command.append(intermediate_audio)  # Добавление выходного файла
+
+        # Выполнение команды
         try:
-            # Создаем временный список для всех аудиофайлов
-            temp_list = "file_list.txt"
-            with open(temp_list, 'w', encoding='utf-8') as f:
-                for file_path in file_paths:
-                    f.write(f"file '{file_path}'\n")
-
-            # Подготавливаем команду для ffmpeg
-            command = [
-                self.ffmpeg_path, '-y', '-f', 'concat', '-safe', '0', '-i', temp_list,
-                '-b:a', bitrate, '-vn', '-c:a', 'aac'
-            ]
+            process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, stderr = process.communicate()
+            if process.returncode != 0:
+                raise RuntimeError(f"Ошибка при объединении аудио файлов: {stderr.decode('utf-8')}")
+            print(stdout.decode('utf-8'))
+        except Exception as e:
+            raise RuntimeError(f"Ошибка при выполнении команды: {e}")
 
 
-            # Добавляем метаданные к команде (они должны быть ДО вывода файла)
-            if metadata["title"]:
-                command.extend(['-metadata', f"title={metadata['title']}"])
-            if metadata["artist"]:
-                command.extend(['-metadata', f"artist={metadata['artist']}"])
-            if metadata["album"]:
-                command.extend(['-metadata', f"album={metadata['album']}"])
-            if metadata["year"]:
-                command.extend(['-metadata', f"year={metadata['year']}"])
-            if metadata["genre"]:
-                command.extend(['-metadata', f"genre={metadata['genre']}"])
 
-            #Добавляем обложку (если есть)
-            if cover_image:
-                cover_image_path = "cover.jpg"
-                with open(cover_image_path, 'wb') as f:
-                    f.write(cover_image)
-                command.extend(['-i', cover_image_path, '-c:v', 'mjpeg', '-map', '0', '-map', '1'])
+        #Добавляем обложку (если есть)
+        if cover_image:
+            cover_image_path = "cover.jpg"
+            with open(cover_image_path, 'wb') as f:
+                f.write(cover_image)
 
+            # Команда ffmpeg для добавления обложки
+            command = [self.ffmpeg_path, '-i', intermediate_audio, '-i', cover_image_path, '-map', '0:0', '-map', '1:0', '-c:a',
+                       'aac', '-b:a', bitrate, '-c:v', 'mjpeg', '-metadata:s:v', 'title="Album cover"', '-metadata:s:v',
+                       'comment="Cover (Front)"', '-disposition:v', 'attached_pic', '-f', 'mp4']
 
-            # Выходной файл должен быть последним аргументом
             command.append(output_path)
 
-            # Выполняем команду через subprocess
-            process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            try:
+                print('==========')
+                process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-            total_files = len(file_paths)
-            for i, _ in enumerate(file_paths, 1):
-                progress = int((i / total_files) * 100)
-                progress_callback(progress)
+                stdout, stderr = process.communicate()
 
-            stdout, stderr = process.communicate()
+                if process.returncode != 0:
+                    raise RuntimeError(f"Ошибка при объединении аудио файлов: {stderr.decode('utf-8', 'ignore')}")
+                print(stdout.decode('utf-8', 'ignore'))
+            except UnicodeDecodeError as e:
+                print(f"Ошибка кодировки: {e}")
+            except Exception as e:
+                raise RuntimeError(f"Ошибка при выполнении команды: {e}")
 
-            if process.returncode != 0:
-                raise Exception(f"Ошибка при конвертации: {stderr.decode('utf-8')}")
+        #Удаляем временные файлы
+        os.remove(temp_list)
+        if cover_image:
+            os.remove(cover_image_path)
 
-            # Удаляем временные файлы
-            os.remove(temp_list)
-            if cover_image:
-                os.remove(cover_image_path)
 
-        except Exception as e:
-            raise RuntimeError(f"Ошибка при конвертации и объединении файлов: {e}")
+# =======================
+#             # Выполняем команду через subprocess
+#             process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#
+#             total_files = len(file_paths)
+#             for i, _ in enumerate(file_paths, 1):
+#                 progress = int((i / total_files) * 100)
+#                 progress_callback(progress)
+#
+#             stdout, stderr = process.communicate()
+#
+#             if process.returncode != 0:
+#                 raise Exception(f"Ошибка при конвертации: {stderr.decode('utf-8')}")
+#
+#             # Удаляем временные файлы
+#             os.remove(temp_list)
+#             if cover_image:
+#                 os.remove(cover_image_path)
+#
+#         except Exception as e:
+#             raise RuntimeError(f"Ошибка при конвертации и объединении файлов: {e}")
